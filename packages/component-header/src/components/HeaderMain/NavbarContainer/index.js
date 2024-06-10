ee5ace8b228d4b8b9eac0f8b177f315ac0b5f2b5
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-06-10 13:47:01 -0700

    refactor: optimize rendering of HeaderMain component and add padding to UniversalNavbar

--- packages/component-header/src/components/HeaderMain/NavbarContainer/index.js
+++ packages/component-header/src/components/HeaderMain/NavbarContainer/index.js
@@ -16,7 +16,7 @@ const NavbarContainer = () => {
   const [itemOpened, setItemOpened] = useState(undefined);
 
   const handleSetItemOpened = itemId => {
-    setItemOpened(prev => (itemOpened === itemId ? undefined : itemId));
+    setItemOpened(() => (itemOpened === itemId ? undefined : itemId));
   };
 
   const renderItem = (link, index) => {

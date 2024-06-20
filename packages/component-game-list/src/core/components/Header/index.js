5ab326699873801e5f1082178ae83832f38052d5
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-06-20 13:58:09 -0700

    refactor: update SunDevilStoriesSection to correct sportName spelling

--- packages/component-game-list/src/core/components/Header/index.js
+++ packages/component-game-list/src/core/components/Header/index.js
@@ -33,7 +33,7 @@ const Header = forwardRef(
             <div className="d-flex flex-row align-items-end justify-content-between gap-2 py-2">
               <h2 className="m-0">{title}</h2>
               <div className="mt-auto mr-auto d-block d-sm-block d-md-none">
-                <div className="d-flex flex-column flex-sm-column flex-md-row align-items-center gap-0">
+                <div className="d-flex flex-column flex-sm-column flex-md-row align-items-center gap-1">
                   <h5 className="m-0">Presented by:</h5>
                   <Logo src={presentedBy.logo} alt={presentedBy.name} />
                 </div>
@@ -63,7 +63,7 @@ const Header = forwardRef(
             )}
           </div>
           <div className="col-md-4 col-sm-0 mt-auto d-none d-sm-none d-md-block">
-            <div className="d-flex flex-row align-items-center gap-2">
+            <div className="d-flex flex-row align-items-center justify-content-end gap-2">
               <h5>Presented by:</h5>
               <Logo src={presentedBy.logo} alt={presentedBy.name} />
             </div>

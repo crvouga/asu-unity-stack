7a3a8efa40ce868f048641a8982a76f9225baffe
Chris Vouga <crvouga@gmail.com>
2024-07-08 12:37:39 -0700

    refactor: remove unnecessary code and elements from header and section components

--- packages/app-sundevil/src/core/init-component.js
+++ packages/app-sundevil/src/core/init-component.js
@@ -17,6 +17,37 @@ import {
 } from "../components";
 
 /**
+ * @description
+ * Removes all children from the given node that do not match any of the
+ * white-listed CSS selectors.
+ * @param {Element} rootNode
+ * @param {string[]} whiteListCssSelectors
+ * @param {number} [maxDepth=1]
+ */
+const filterDOM = (rootNode, whiteListCssSelectors, maxDepth = 1) => {
+  const matchesWhitelist = element => {
+    return whiteListCssSelectors.some(selector => element.matches(selector));
+  };
+
+  /**
+   * @param {Element} node
+   * @param {number} depth
+   */
+  const filterNode = (node, depth) => {
+    if (depth > maxDepth) return;
+
+    Array.from(node.children).forEach(child => {
+      if (!matchesWhitelist(child)) {
+        node.removeChild(child);
+      } else {
+        filterNode(child, depth + 1);
+      }
+    });
+  };
+
+  filterNode(rootNode, 1);
+};
+/**
  * @typedef {object} RenderInput
  * @property {string} targetSelector - The CSS selector (#id or .class)
  * which identify the <div> element where the footer should be either hydrated
@@ -28,7 +59,15 @@ import {
  * drupal puts other elements inside blocks, we need to render the component
  * inside a child element of the target element so we don't wipe out the other
  * elements inserted by drupal.
+ * @property {string[]} [renderWithinChildWhiteList] - An array of CSS selectors that
+ * are allowed to be rendered inside the child element. This is used in
+ * conjunction with renderWithinChild.
+ * @property {string} [renderWithinChildReactId] - The id of the child element
+ * to render the component inside. This is used in conjunction with
+ * renderWithinChild.
  */
+
+const REACT_CHILD_TARGET_ID = "react-child-target";
 
 /**
  * @type {(input: RenderInput) => void}
@@ -38,6 +77,8 @@ export const RenderReact = ({
   props,
   targetSelector,
   renderWithinChild = true,
+  renderWithinChildReactId = REACT_CHILD_TARGET_ID,
+  renderWithinChildWhiteList = [],
 }) => {
   const target = document.querySelector(targetSelector);
 
@@ -46,14 +87,24 @@ export const RenderReact = ({
   }
 
   if (renderWithinChild) {
-    const targetChild = document.createElement("div");
-    target.appendChild(targetChild);
+    if (!renderWithinChildWhiteList.includes(`#${renderWithinChildReactId}`)) {
+      renderWithinChildWhiteList.push(`#${renderWithinChildReactId}`);
+    }
+    const targetChild = target.querySelector(`#${renderWithinChildReactId}`);
 
-    // @ts-ignore
+    if (!targetChild) {
+      const targetChildNew = document.createElement("div");
+      targetChildNew.id = renderWithinChildReactId;
+      target.appendChild(targetChildNew);
+      filterDOM(target, renderWithinChildWhiteList);
+      ReactDOM.render(React.createElement(component, props), targetChildNew);
+      return;
+    }
+
+    filterDOM(target, renderWithinChildWhiteList);
     ReactDOM.render(React.createElement(component, props), targetChild);
     return;
   }
-
   // @ts-ignore
   ReactDOM.render(React.createElement(component, props), target);
 };

003b79bce168cbb8aa486665f8b24533c40c5908
Chris Vouga <crvouga@gmail.com>
2024-11-24 20:43:56 -0700

    feat: enhance SponsorLogo component to dynamically adjust height and integrate GoogleAd; improve prop handling

--- packages/app-sundevil/src/components/Header/sponsor-logo/SponsorLogo.js
+++ packages/app-sundevil/src/components/Header/sponsor-logo/SponsorLogo.js
@@ -1,8 +1,39 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+
+import { GoogleAd } from "../../../google-ads/GoogleAd";
+
+const Root = styled.div`
+  height: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  height: 100%;
+  min-height: 100%;
+  max-height: 100%;
+`;
 
 /**
  * @type {React.FC<import("./props").SponsorLogoProps>}
  */
-export const SponsorLogo = () => {
-  return <div>GoogleAd</div>;
+export const SponsorLogo = props => {
+  const { googleAdBody, googleAdHead } = props;
+  /**
+   * @type {React.RefObject<HTMLDivElement | undefined>}
+   */
+  const ref = useRef();
+  useEffect(() => {
+    if (ref.current) {
+      const parent = ref.current.parentElement;
+      const parentHeight = parent.clientHeight;
+      ref.current.style.height = `${parentHeight}px`;
+    }
+  }, []);
+  return (
+    <Root ref={ref}>
+      <GoogleAd googleAdBody={googleAdBody} googleAdHead={googleAdHead} />
+    </Root>
+  );
 };

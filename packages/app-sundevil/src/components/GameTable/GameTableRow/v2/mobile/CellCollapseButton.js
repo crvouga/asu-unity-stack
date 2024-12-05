e4f324420b346559cfda50096e9dbbcbdb59b8c5
Chris Vouga <crvouga@gmail.com>
2024-12-04 20:03:45 -0700

    feat: add GA tracking to CellCollapseButton for collapse state changes

--- packages/app-sundevil/src/components/GameTable/GameTableRow/v2/mobile/CellCollapseButton.js
+++ packages/app-sundevil/src/components/GameTable/GameTableRow/v2/mobile/CellCollapseButton.js
@@ -3,6 +3,7 @@
 import React from "react";
 import styled from "styled-components";
 
+import { trackGAEvent } from "../../../../../track-ga/track-ga-event";
 import { CollapseIcon } from "../../../../CollapseIcon/CollapseIcon";
 
 const Root = styled.button`
@@ -21,7 +22,7 @@ export const CellCollapseButton = (
   /**
    * @type {{ sectionName:string, open: boolean; onClick: () => void; }}
    */
-  { open, onClick }
+  { open, onClick, sectionName }
 ) => {
   return (
     <Root
@@ -29,6 +30,17 @@ export const CellCollapseButton = (
       onClick={() => {
         if (typeof onClick === "function") {
           onClick?.();
+          const openNew = !open;
+          trackGAEvent({
+            event: "collapse",
+            action: openNew ? "open" : "close",
+            name: "onclick",
+            type: "click",
+            region: "main content",
+            section: sectionName,
+            text: "collapse",
+            component: "text",
+          });
         }
       }}
       style={{
